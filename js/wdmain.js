362469398157f8936f3be2481f805c4d04f03198
Julia Marques <jcastro.cg@gmail.com>
2017-05-09 04:41:27 -0700

    Major changes - 5th floor finished

--- js/wdmain.js
+++ js/wdmain.js
@@ -13,7 +13,34 @@ $(document).ready(function() {
 });
 
 
+/*
 
+var zombie = $('#zombie'),
+	pLocs = [0, -250, -500, -750, -1000, -1250, -1500]
+	curFrm = 0,
+	lastStep=0;
+
+skrollr.init({
+	beforerender: function(o){
+		// If the user has scrolled 50 pixels down
+		// since the last time we shifted the background image,
+		// we must be moving forward, so move to the next frame
+		// in the spritesheet
+		if(o.curTop > lastStep + 500) {
+			if (curFrm>=6){ curFrm=-1; }
+			zombie.css('background-position', pLocs[curFrm++] + 'px 0px');
+			lastStep = o.curTop;
+		// If the user has scrolled 50 pixels up,
+		// we’re moving backwards, so move to the previous frame
+		} else if(o.curTop < lastStep - 500) {
+			if (curFrm<=0){ curFrm=7; }
+			zombie.css('background-position', pLocs[curFrm--] + 'px 0px');
+			lastStep = o.curTop;
+		}
+	}
+});
+
+*/
 
 var zombie = $('#zombie'),
 	pLocs = [0, -250, -500, -750, -1000, -1250, -1500],
@@ -34,11 +61,11 @@ skrollr.init({
 			animationCycle=4;
 			backPosY= '-190px';
 		}
-		if(o.curTop > lastStep + 50) {
+		if(o.curTop > lastStep + 400) {
 			if (curFrm>=animationCycle-1){ curFrm=-1; }
 			zombie.css('background-position', pLocs[++curFrm] + 'px ' + backPosY);
 			lastStep=o.curTop;
-		} else if(o.curTop < lastStep - 50) {
+		} else if(o.curTop < lastStep - 400) {
 			if (curFrm<=0){ curFrm=animationCycle; }
 			zombie.css('background-position', pLocs[--curFrm] + 'px ' + backPosY);
 			lastStep = o.curTop;
